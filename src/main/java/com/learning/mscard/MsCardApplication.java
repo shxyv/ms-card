17546ed436db3eeb2e609a7b9796ab80f5d01aae
sixiyevih <ilgar.shixiyevv@gmail.com>
2023-02-20 00:21:32 +0400

    registered app to consul discovery server

--- src/main/java/com/learning/mscard/MsCardApplication.java
+++ src/main/java/com/learning/mscard/MsCardApplication.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@EnableDiscoveryClient
 public class MsCardApplication implements CommandLineRunner {
 
     private final CardRepository cardRepository;
